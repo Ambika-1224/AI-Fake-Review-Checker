78f99a4e7079d7bc2c6ea29c52021bfba2b11286
Ambika <ambikachoudhary7877@gmail.com>
2026-07-07 19:41:15 +0530

    fix api key for streamlit cloud

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+if not GEMINI_API_KEY:
+    GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
